ec043016807f7890617dd7df2bd2e48ec2be950e
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2022-12-22 18:19:42 -0500

    Se termian el ejercicio con tres tareas
    ahora se va cambia el excutor de Executors.newSingleThreadExecutor()
    o otro que maneja multiple instancias.

--- src/work/oscarramos/hilos/ejemploexecutor/EjemploExecutorFuture2.java
+++ src/work/oscarramos/hilos/ejemploexecutor/EjemploExecutorFuture2.java
@@ -2,7 +2,7 @@ package work.oscarramos.hilos.ejemploexecutor;
 
 import java.util.concurrent.*;
 
-public class EjemploExecutorFuture {
+public class EjemploExecutorFuture2 {
     public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
         ExecutorService excutor = Executors.newSingleThreadExecutor();
 
@@ -18,14 +18,26 @@ public class EjemploExecutorFuture {
             System.out.println("Se finalizo la tarea...");
             return "Algun resultado importante de la tarea";
         };
+
+        Callable<Integer> tarea2 = () -> {
+            System.out.println("Iniciando tarea 2 ....");
+            TimeUnit.SECONDS.sleep(3);
+            return 10;
+        };
+
         Future<String> restualtado = excutor.submit(tarea);
+        Future<String> restualtado2 = excutor.submit(tarea);
+        Future<Integer> restualtado3 = excutor.submit(tarea2);
         excutor.shutdown();
         System.out.println("Continuando con la ejecucion del metodo main");
 
         //System.out.println(restualtado.isDone());
-        while(!restualtado.isDone()){
-            System.out.println("Ejecutando tarea ....");
-            TimeUnit.MILLISECONDS.sleep(1500);
+        while(!(restualtado.isDone() && restualtado2.isDone() && restualtado3.isDone())){
+            System.out.println(String.format("Resultado1: %s - resultado2: %s - Resultado3: %s",
+                    restualtado.isDone()? "finalizo": "en proceso",
+                    restualtado2.isDone()? "finalizo":"en proceso",
+                    restualtado3.isDone()? "finalizo":"en proceso"));
+            TimeUnit.MILLISECONDS.sleep(1000);
         }
         System.out.println("Obtenemos el resultado de la tarea : " + restualtado.get());
         System.out.println(restualtado.isDone());
