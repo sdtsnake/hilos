49e2e600dda6c5291af3a2c57cc24adde2f589fd
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2022-12-22 22:00:20 -0500

    se termina el ejercicio de hilos con excutor
    se implementa un AtomicInteger para controlar
    el futuro y se termina el ejercicio

--- src/work/oscarramos/hilos/ejemploexecutor/EjemploScheduledExecutorServicePeriodo.java
+++ src/work/oscarramos/hilos/ejemploexecutor/EjemploScheduledExecutorServicePeriodo.java
@@ -1,6 +1,7 @@
 package work.oscarramos.hilos.ejemploexecutor;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class EjemploScheduledExecutorServicePeriodo {
     public static void main(String[] args) throws InterruptedException {
@@ -9,21 +10,30 @@ public class EjemploScheduledExecutorServicePeriodo {
 
         System.out.println("Alguna tarea del main");
 
-        CountDownLatch lock = new CountDownLatch(5);
+        AtomicInteger contador = new AtomicInteger(5);
+        //CountDownLatch lock = new CountDownLatch(5);
 
         Future<?> future = executur.scheduleAtFixedRate(()->{
             try {
                 TimeUnit.MILLISECONDS.sleep(1000);
-                lock.countDown();
+                //lock.countDown();
+                contador.getAndDecrement();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             System.out.println("Hola mundo tarea ..... ");
         },1000,2000, TimeUnit.MILLISECONDS);
 
-        lock.await();
-        future.cancel(true);
+        //lock.await();
+        //future.cancel(true);
         //TimeUnit.SECONDS.sleep(10);
+
+        while (contador.get() >= 0 ){
+            if(contador.get()==0){
+                future.cancel(true);
+                break;
+            }
+        }
 
         System.out.println("Alguna otra tarea del main....");
         executur.shutdown();
